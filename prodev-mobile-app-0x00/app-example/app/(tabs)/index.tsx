f252f594a1877aaf32561c3d9685b8b75145609c
Shadrack Okyere <masterokyere100@gmail.com>
2026-01-11 23:11:19 +0000

    Update index.tsx

--- prodev-mobile-app-0x00/app-example/app/(tabs)/index.tsx
+++ prodev-mobile-app-0x00/app-example/app/(tabs)/index.tsx
@@ -1,1 +1,9 @@
+import { Text, View } from "react-native";
 
+export default function HomeScreen() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-lg font-bold">First App Created</Text>
+    </View>
+  );
+}
